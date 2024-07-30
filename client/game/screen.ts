09fe429277ce3246b0aa66c94ef56f0c20ed8a90
prampey <prudhvi.rampey@gmail.com>
2024-07-30 18:23:56 +0530

    feat: set determinstic random generator and fixed window size

--- client/game/screen.ts
+++ client/game/screen.ts
@@ -1,5 +1,9 @@
 import { Draw } from './draw';
 
+export const WIDTH: number = 960;
+export const HEIGHT: number = 720;
+export const OBJECT_SCALE: number = .75;
+
 export class Screen implements Rect {
 
     canvas: HTMLCanvasElement;
@@ -32,32 +36,18 @@ export class Screen implements Rect {
     }
 
     init() {
-        this.canvas.width = document.body.clientWidth; 
-        this.canvas.height = document.body.clientHeight; 
+        this.canvas.width = WIDTH; 
+        this.canvas.height = HEIGHT; 
         this.width = this.canvas.width;
         this.height = this.canvas.height;
         this.width2 = this.width / 2;
         this.height2 = this.height / 2;
 
-        if (this.width >= 1280) {
-            this._fontXL = 60;
-            this._fontL = 30;
-            this._fontM = 24; 
-            this._fontS = 12;
-            this._objectScale = 1;
-        } else if (this.width >= 800) {
-            this._fontXL = 48;
-            this._fontL = 24;
-            this._fontM = 18; 
-            this._fontS = 10;
-            this._objectScale = .75;
-        } else {
-            this._fontXL = 36;
-            this._fontL = 12;
-            this._fontM = 10; 
-            this._fontS = 6;
-            this._objectScale = .5;
-        }
+        this._fontXL = 48;
+        this._fontL = 24;
+        this._fontM = 18; 
+        this._fontS = 10;
+        this._objectScale = OBJECT_SCALE;
 
         this._pointSize = 4 * this._objectScale;
 
