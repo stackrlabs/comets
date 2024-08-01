405d9376d5bd92000fb48f92379e001b0510b4f4
prampey <prudhvi.rampey@gmail.com>
2024-08-01 14:02:27 +0530

    feat: remove render based game logic updates
    - disable slowmo
    - disable ship trail reduction

--- client/game/ship.ts
+++ client/game/ship.ts
@@ -17,205 +17,206 @@ const MAX_BULLETS: number = 10;
 const VELOCITY = 150 * OBJECT_SCALE;
 
 class Flame extends Object2D {
-  constructor(x: number, y: number) {
-    super(x, y);
+	constructor(x: number, y: number) {
+		super(x, y);
 
-    this.points = [
-      { x: 5, y: 8 },
-      { x: 0, y: 20 },
-      { x: -5, y: 8 },
-    ];
-  }
+		this.points = [
+			{ x: 5, y: 8 },
+			{ x: 0, y: 20 },
+			{ x: -5, y: 8 },
+		];
+	}
 
-  update() {}
+	update() {}
 
-  render(screen: Screen) {
-    this.draw(screen, false);
-  }
+	render(screen: Screen) {
+		this.draw(screen, false);
+	}
 }
 
 export class Ship extends Object2D {
-  private moving: boolean = false;
-  private bulletCount: number = 0;
-  private bulletTimer: number = 0;
-  private flame: Flame;
-  public shield: number = 1;
-  public trails = [];
+	private moving: boolean = false;
+	private bulletCount: number = 0;
+	private bulletTimer: number = 0;
+	private flame: Flame;
+	public shield: number = 1;
+	public trails = [];
 
-  constructor(x: number, y: number) {
-    super(x, y);
-    this.flame = new Flame(x, y);
-    this.points = [
-      { x: 0, y: -15 },
-      { x: 10, y: 10 },
-      { x: 5, y: 5 },
-      { x: -5, y: 5 },
-      { x: -10, y: 10 },
-    ];
+	constructor(x: number, y: number) {
+		super(x, y);
+		this.flame = new Flame(x, y);
+		this.points = [
+			{ x: 0, y: -15 },
+			{ x: 10, y: 10 },
+			{ x: 5, y: 5 },
+			{ x: -5, y: 5 },
+			{ x: -10, y: 10 },
+		];
 
-    this.angle = 270;
-  }
+		this.angle = 270;
+	}
 
-  render(screen: Screen) {
-    this.draw(screen);
+	render(screen: Screen) {
+		this.draw(screen);
 
-    if (this.moving && random(1, 10) % 2 === 0) {
-      this.flame.draw(screen, false);
-    }
+		if (this.moving && random(1, 10) % 2 === 0) {
+			this.flame.draw(screen, false);
+		}
 
-    if (this.trails.length) {
-      this.trails.forEach((trail) => {
-        if (trail.alpha > 0) {
-          screen.draw.shape(
-            trail.points,
-            trail.x,
-            trail.y,
-            `rgba(255,0,255,${trail.alpha})`,
-            true
-          );
-          screen.draw.shape(
-            trail.points,
-            trail.x - 1,
-            trail.y - 1,
-            `rgba(0,255,255,${trail.alpha})`,
-            true
-          );
-          trail.alpha -= 0.1;
-        }
-      });
-    }
-  }
+		if (this.trails.length) {
+			this.trails.forEach((trail) => {
+				if (trail.alpha > 0) {
+					screen.draw.shape(
+						trail.points,
+						trail.x,
+						trail.y,
+						`rgba(255,0,255,${trail.alpha})`,
+						true
+					);
+					screen.draw.shape(
+						trail.points,
+						trail.x - 1,
+						trail.y - 1,
+						`rgba(0,255,255,${trail.alpha})`,
+						true
+					);
+					// Disabling trail update for now
+					// trail.alpha -= 0.1;
+				}
+			});
+		}
+	}
 
-  update(dt: number, inputs?: VirtualInput) {
-    this.move(dt);
-    this.flame.move(dt);
+	update(dt: number, inputs?: VirtualInput) {
+		this.move(dt);
+		this.flame.move(dt);
 
-    if (inputs.isThrust) {
-      this.moving = true;
-      this.thrust();
-    } else {
-      this.moving = false;
-    }
+		if (inputs.isThrust) {
+			this.moving = true;
+			this.thrust();
+		} else {
+			this.moving = false;
+		}
 
-    if (inputs.wasRotateLeft) {
-      this.rotate(-1);
-    }
+		if (inputs.wasRotateLeft) {
+			this.rotate(-1);
+		}
 
-    if (inputs.isRotateLeft) {
-      this.rotate(-ROTATION);
-    }
+		if (inputs.isRotateLeft) {
+			this.rotate(-ROTATION);
+		}
 
-    if (inputs.wasRotateRight) {
-      this.rotate(1);
-    }
+		if (inputs.wasRotateRight) {
+			this.rotate(1);
+		}
 
-    if (inputs.isRotateRight) {
-      this.rotate(ROTATION);
-    }
+		if (inputs.isRotateRight) {
+			this.rotate(ROTATION);
+		}
 
-    if (inputs.isFire) {
-      this.fire();
-    }
+		if (inputs.isFire) {
+			this.fire();
+		}
 
-    if (inputs.wasHyperspace) {
-      this.hyperspace();
-    }
+		if (inputs.wasHyperspace) {
+			this.hyperspace();
+		}
 
-    if (this.bulletTimer >= 0) {
-      this.bulletTimer -= dt;
-    }
+		if (this.bulletTimer >= 0) {
+			this.bulletTimer -= dt;
+		}
 
-    if (
-      this.moving &&
-      (Math.abs(this.velocity.x) > 200 || Math.abs(this.velocity.y) > 200)
-    ) {
-      this.trails.push({
-        points: [...this.points],
-        x: this.origin.x,
-        y: this.origin.y,
-        alpha: 0.5,
-      });
-    } else {
-      this.trails.length = 0;
-    }
+		if (
+			this.moving &&
+			(Math.abs(this.velocity.x) > 200 || Math.abs(this.velocity.y) > 200)
+		) {
+			this.trails.push({
+				points: [...this.points],
+				x: this.origin.x,
+				y: this.origin.y,
+				alpha: 0.5,
+			});
+		} else {
+			this.trails.length = 0;
+		}
 
-    // slow down ship over time
-    if (!this.moving) {
-      this.velocity.friction(FRICTION);
-      this.flame.velocity = this.velocity;
-    }
-  }
+		// slow down ship over time
+		if (!this.moving) {
+			this.velocity.friction(FRICTION);
+			this.flame.velocity = this.velocity;
+		}
+	}
 
-  rotate(n: number) {
-    super.rotate(n);
-    this.flame.rotate(n);
-  }
+	rotate(n: number) {
+		super.rotate(n);
+		this.flame.rotate(n);
+	}
 
-  private thrust() {
-    const v = Vector.fromAngle(this.angle, VELOCITY * ACCELERATION);
+	private thrust() {
+		const v = Vector.fromAngle(this.angle, VELOCITY * ACCELERATION);
 
-    if (this.velocity.magnitude < MAX_ACCELERATION) {
-      this.velocity.add(v);
-      this.flame.velocity = this.velocity;
-    }
+		if (this.velocity.magnitude < MAX_ACCELERATION) {
+			this.velocity.add(v);
+			this.flame.velocity = this.velocity;
+		}
 
-    // thrust.play();
-  }
+		// thrust.play();
+	}
 
-  private fire() {
-    if (this.bulletTimer <= 0 && this.bulletCount < MAX_BULLETS) {
-      // fire.play();
+	private fire() {
+		if (this.bulletTimer <= 0 && this.bulletCount < MAX_BULLETS) {
+			// fire.play();
 
-      this.bulletTimer = BULLET_TIME;
-      this.bulletCount++;
+			this.bulletTimer = BULLET_TIME;
+			this.bulletCount++;
 
-      const velocity = Vector.fromAngle(this.angle);
-      const bullet = new Bullet(this.origin, velocity, 1);
+			const velocity = Vector.fromAngle(this.angle);
+			const bullet = new Bullet(this.origin, velocity, 1);
 
-      bullet.on("expired", () => {
-        this.bulletCount--;
-      });
+			bullet.on("expired", () => {
+				this.bulletCount--;
+			});
 
-      // move bullet to nose of ship
-      const bv = bullet.velocity.copy();
-      bv.scale(20, 20);
-      bullet.origin.add(bv);
+			// move bullet to nose of ship
+			const bv = bullet.velocity.copy();
+			bv.scale(20, 20);
+			bullet.origin.add(bv);
 
-      // adjust for speed of ship if bullets and ship are moving in same general direction
-      let speed = 0;
-      const dot = this.velocity.dot(bullet.velocity);
+			// adjust for speed of ship if bullets and ship are moving in same general direction
+			let speed = 0;
+			const dot = this.velocity.dot(bullet.velocity);
 
-      if (dot > 0) {
-        speed = this.velocity.magnitude;
-      }
+			if (dot > 0) {
+				speed = this.velocity.magnitude;
+			}
 
-      speed = Math.max(BULLET_SPEED, speed + BULLET_SPEED);
+			speed = Math.max(BULLET_SPEED, speed + BULLET_SPEED);
 
-      bullet.velocity.scale(speed, speed);
+			bullet.velocity.scale(speed, speed);
 
-      // kick back
-      let kba = (this.angle + 180) % 360;
-      let kbv = Vector.fromAngle(kba, 5);
+			// kick back
+			let kba = (this.angle + 180) % 360;
+			let kbv = Vector.fromAngle(kba, 5);
 
-      this.origin.add(kbv);
-      this.flame.origin.add(kbv);
+			this.origin.add(kbv);
+			this.flame.origin.add(kbv);
 
-      this.trigger("fire", bullet);
-    }
-  }
+			this.trigger("fire", bullet);
+		}
+	}
 
-  hyperspace() {
-    let x = random(40, WIDTH - 40);
-    let y = random(40, HEIGHT - 40);
+	hyperspace() {
+		let x = random(40, WIDTH - 40);
+		let y = random(40, HEIGHT - 40);
 
-    this.velocity = new Vector(0, 0);
-    this.flame.velocity = this.velocity;
+		this.velocity = new Vector(0, 0);
+		this.flame.velocity = this.velocity;
 
-    this.x = this.flame.x = x;
-    this.y = this.flame.y = y;
-  }
+		this.x = this.flame.x = x;
+		this.y = this.flame.y = y;
+	}
 
-  destroy() {
-    this.trigger("expired");
-  }
+	destroy() {
+		this.trigger("expired");
+	}
 }
--- client/game/world.ts
+++ client/game/world.ts
@@ -25,370 +25,374 @@ let explosionScores: number[] = [];
 
 const NUM_OF_LIVES = 1;
 export class World {
-  level = 1;
-  extraLifeScore = 0;
-  score = 0;
-  lives = NUM_OF_LIVES;
-
-  ship: Ship;
-  shipBullets: Bullet[] = [];
-  alien: Alien;
-  alienBullets: Bullet[] = [];
-  shockwaves: Shockwave[] = [];
-  rocks: Rock[] = [];
-
-  // things with no collision detection - markers, explosions, flash, etc.
-  scenery: IGameState[] = [];
-
-  shipTimer: number = 0;
-  alienTimer: number = 0;
-  levelTimer: number = 0;
-  gameOverTimer: number = 0;
-  shakeTimer: number = 0;
-  powerupTimer: number = 0;
-  dramaticPauseTimer: number = 0;
-  slowMoTimer: SlowMoTimer = null;
-
-  gameOver: boolean = false;
-  started: boolean = false;
-
-  constructor() {}
-
-  get objects(): any {
-    return [
-      this.ship,
-      this.alien,
-      ...this.shipBullets,
-      ...this.alienBullets,
-      ...this.rocks,
-      ...this.shockwaves,
-      ...this.scenery,
-    ];
-  }
-
-  update(dt: number, inputs: VirtualInput) {
-    if (this.slowMoTimer) {
-      dt = this.slowMoTimer.adjust(dt);
-    }
-
-    if (this.dramaticPauseTimer > 0) {
-      this.dramaticPauseTimer--;
-      return;
-    }
-
-    // shaky cam
-    if (this.shakeTimer > 0) {
-      this.shakeTimer -= dt;
-    }
-
-    this.objects.forEach((obj) => {
-      if (obj) {
-        obj.update(dt, inputs);
-      }
-    });
-  }
-
-  render(screen: Screen, dt?: number) {
-    if (this.slowMoTimer) {
-      dt = this.slowMoTimer.adjust(dt);
-    }
-
-    if (this.shakeTimer > 0) {
-      screen.preShake();
-    }
-
-    this.objects.forEach((obj) => {
-      if (obj) {
-        obj.render(screen, dt);
-      }
-    });
-
-    if (this.shakeTimer > 0) {
-      screen.postShake();
-    }
-  }
-
-  startLevel() {
-    this.level++;
-    this.levelTimer = 0;
-    this.powerupTimer = 0;
-
-    if (!this.alienTimer) {
-      this.alienTimer = random(10, 15);
-    }
-
-    this.scenery.length = 0;
-    this.shipBullets.forEach((bullet) => bullet.destroy());
-
-    this.addRocks();
-  }
-
-  private addRocks() {
-    this.rocks = createRocks(this.level);
-  }
-
-  addShip(x: number, y: number) {
-    this.ship = new Ship(x, y);
-
-    this.ship.on("fire", (ship, bullet) => {
-      bullet.on("expired", () => {
-        this.shipBullets = this.shipBullets.filter((x) => x !== bullet);
-      });
-
-      this.shipBullets.push(bullet);
-    });
-
-    this.ship.on("expired", () => {
-      this.lives--;
-      this.ship = null;
-      this.shipBullets.length = 0;
-    });
-  }
-
-  createExplosion(
-    obj: Object2D,
-    size: number = 100,
-    multiplier: number = 1
-  ): { explosion: Explosion; shockwave: Shockwave } {
-    if (!obj) {
-      return;
-    }
-
-    const explosion = new Explosion(obj.origin.x, obj.origin.y, size);
-    explosionCount++;
-
-    explosionScores.push(obj.score);
-
-    if (explosionScores.length > maxExplosionThreshold) {
-      explosionScores.pop();
-    }
-
-    if (explosionCount > maxExplosionCount) {
-      maxExplosionCount = explosionCount;
-
-      if (maxExplosionCount > maxExplosionThreshold) {
-        explosionCount = 0;
-        console.log("MAX DAMAGE ACHEIVEMENT");
-
-        this.setSlowMo(0.25, 4);
-
-        let bonus = 0;
-        explosionScores.forEach((v) => (bonus += v));
-        bonus *= 5;
-
-        const achievement = new Achievement(`MASSIVE DAMAGE`, bonus);
-        this.addScenery(achievement);
-        this.addScore(achievement, "achievement");
-
-        //const marker = new ScoreMarker(obj, `+${bonus}`);
-        //this.addScenery(marker);
-
-        // Track score of each explosion and display total points with achievement
-
-        maxExplosionThreshold += 10;
-      }
-    }
-
-    explosion.on("expired", () => {
-      explosionCount--;
-    });
-
-    this.addScenery(explosion);
-
-    const shockwave = new Shockwave(
-      obj.origin.x,
-      obj.origin.y,
-      obj.velocity,
-      size,
-      multiplier
-    );
-
-    shockwave.on("expired", () => {
-      this.shockwaves = this.shockwaves.filter((x) => x !== shockwave);
-    });
-
-    this.shockwaves.push(shockwave);
-
-    return {
-      explosion,
-      shockwave,
-    };
-  }
-
-  setSlowMo(time: number, factor: number) {
-    if (!this.slowMoTimer) {
-      this.slowMoTimer = new SlowMoTimer(time, factor);
-      this.slowMoTimer.on("expired", () => (this.slowMoTimer = null));
-    }
-  }
-
-  shipDestroyed() {
-    if (this.ship) {
-      // largeExplosion.play();
-      this.createExplosion(this.ship);
-      this.addFlash(5);
-      this.ship.destroy();
-      this.setSlowMo(0.25, 8);
-    }
-  }
-
-  alienDestroyed() {
-    if (this.alien) {
-      this.addFlash(5);
-      this.createExplosion(this.alien);
-      this.alien.destroy();
-    }
-  }
-
-  addFlash(frames: number) {
-    const flash = new Flash(frames);
-    this.addScenery(flash);
-  }
-
-  addScenery(obj: any) {
-    obj.on("expired", () => {
-      this.scenery = this.scenery.filter((x) => x !== obj);
-    });
-
-    this.scenery.push(obj);
-  }
-
-  rockDestroyed(rock: Rock, multiplier: number = 1) {
-    let boom = this.createExplosion(rock, rock.size * 5, multiplier);
-    let debris = rock.split();
-
-    this.rocks = this.rocks.filter((x) => x !== rock);
-    this.rocks.push(...debris);
-
-    this.shockwaves.forEach((shockwave) => {
-      shockwave.rocks = shockwave.rocks.filter((x) => x !== rock);
-    });
-
-    boom.shockwave.rocks = debris;
-
-    rock = null;
-  }
-
-  addAlien() {
-    const lvl = Math.min(this.level, 14);
-    let little = false;
-    let alienSound = largeAlien;
-
-    if (this.score >= 40000) {
-      little = true;
-    } else {
-      switch (lvl) {
-        case 7:
-          little = this.levelTimer > 60 && random(1, 3) === 2;
-          break;
-        case 8:
-          little = this.levelTimer > 30 && random(1, 10) % 2 === 0;
-          break;
-        default:
-          little = random(1, 10) <= lvl + 2;
-          break;
-      }
-    }
-
-    if (little) {
-      alienSound = smallAlien;
-      this.alien = new SmallAlien(this.ship);
-    } else {
-      this.alien = new BigAlien();
-    }
-
-    // alienSound.play();
-
-    this.alien.on("expired", () => {
-      // alienFire.stop();
-      // alienSound.stop();
-      // largeExplosion.play();
-      this.alien = null;
-      this.alienBullets.forEach((b) => b.destroy());
-      this.alienBullets.length = 0;
-    });
-
-    this.alien.on("fire", (alien, bullet: Bullet) => {
-      // alienFire.play();
-
-      bullet.on("expired", () => {
-        this.alienBullets = this.alienBullets.filter((x) => x !== bullet);
-      });
-
-      this.alienBullets.push(bullet);
-    });
-  }
-
-  // TODO: remove second argument
-  addScore(obj: Object2D, name: string) {
-    console.log(obj.score, name);
-    this.score += obj.score;
-    this.extraLifeScore += obj.score;
-
-    if (this.extraLifeScore >= EXTRA_LIFE) {
-      this.lives++;
-      this.extraLifeScore -= EXTRA_LIFE;
-      // extraLife.play();
-    }
-
-    this.addScenery(new ScoreMarker(obj, `${obj.score}`));
-  }
-
-  addPowerup() {
-    // getPowerup.play();
-  }
-
-  shake() {
-    if (this.shakeTimer <= 0.0) {
-      this.shakeTimer = SHAKE_TIME;
-    }
-  }
-
-  tryPlaceShip(dt) {
-    this.shipTimer += dt;
-
-    if (this.shipTimer <= 2) {
-      return;
-    }
-
-    let rect: Rect = SHIP_RECT;
-
-    let collided = false;
-
-    this.rocks.forEach((rock) => {
-      collided = collided || rock.collided(rect);
-    });
-
-    if (this.alien) {
-      collided = collided || this.alien.collided(rect);
-    }
-
-    if (!collided) {
-      this.shipTimer = 0;
-      this.addShip(WIDTH / 2, HEIGHT / 2);
-    }
-  }
-
-  updateAlienTimer(dt: number) {
-    if (!this.alien) {
-      this.alienTimer -= dt;
-
-      if (this.alienTimer <= 0) {
-        this.addAlien();
-        this.alienTimer = random(10, 15);
-      }
-    }
-  }
-
-  shouldTryToPlaceShip(): boolean {
-    return !!this.shipTimer || (!this.ship && !!this.lives);
-  }
-
-  shouldCheckForNextLevel(): boolean {
-    return !this.rocks.length && !!this.lives;
-  }
-
-  shouldCheckCollisions(): boolean {
-    return !!this.ship || !!this.shipBullets.length;
-  }
+	level = 1;
+	extraLifeScore = 0;
+	score = 0;
+	lives = NUM_OF_LIVES;
+
+	ship: Ship;
+	shipBullets: Bullet[] = [];
+	alien: Alien;
+	alienBullets: Bullet[] = [];
+	shockwaves: Shockwave[] = [];
+	rocks: Rock[] = [];
+
+	// things with no collision detection - markers, explosions, flash, etc.
+	scenery: IGameState[] = [];
+
+	shipTimer: number = 0;
+	alienTimer: number = 0;
+	levelTimer: number = 0;
+	gameOverTimer: number = 0;
+	shakeTimer: number = 0;
+	powerupTimer: number = 0;
+	dramaticPauseTimer: number = 0;
+	slowMoTimer: SlowMoTimer = null;
+
+	gameOver: boolean = false;
+	started: boolean = false;
+
+	constructor() {}
+
+	get objects(): any {
+		return [
+			this.ship,
+			this.alien,
+			...this.shipBullets,
+			...this.alienBullets,
+			...this.rocks,
+			...this.shockwaves,
+			...this.scenery,
+		];
+	}
+
+	update(dt: number, inputs: VirtualInput) {
+		// Disabling slowmo for now
+		// if (this.slowMoTimer) {
+		//   dt = this.slowMoTimer.adjust(dt);
+		// }
+
+		if (this.dramaticPauseTimer > 0) {
+			this.dramaticPauseTimer--;
+			return;
+		}
+
+		// shaky cam
+		if (this.shakeTimer > 0) {
+			this.shakeTimer -= dt;
+		}
+
+		this.objects.forEach((obj) => {
+			if (obj) {
+				obj.update(dt, inputs);
+			}
+		});
+	}
+
+	render(screen: Screen, dt?: number) {
+		// Disabling slowmo for now
+		// if (this.slowMoTimer) {
+		//   dt = this.slowMoTimer.adjust(dt);
+		// }
+
+		if (this.shakeTimer > 0) {
+			screen.preShake();
+		}
+
+		this.objects.forEach((obj) => {
+			if (obj) {
+				obj.render(screen, dt);
+			}
+		});
+
+		if (this.shakeTimer > 0) {
+			screen.postShake();
+		}
+	}
+
+	startLevel() {
+		this.level++;
+		this.levelTimer = 0;
+		this.powerupTimer = 0;
+
+		if (!this.alienTimer) {
+			this.alienTimer = random(10, 15);
+		}
+
+		this.scenery.length = 0;
+		this.shipBullets.forEach((bullet) => bullet.destroy());
+
+		this.addRocks();
+	}
+
+	private addRocks() {
+		this.rocks = createRocks(this.level);
+	}
+
+	addShip(x: number, y: number) {
+		this.ship = new Ship(x, y);
+
+		this.ship.on("fire", (ship, bullet) => {
+			bullet.on("expired", () => {
+				this.shipBullets = this.shipBullets.filter((x) => x !== bullet);
+			});
+
+			this.shipBullets.push(bullet);
+		});
+
+		this.ship.on("expired", () => {
+			this.lives--;
+			this.ship = null;
+			this.shipBullets.length = 0;
+		});
+	}
+
+	createExplosion(
+		obj: Object2D,
+		size: number = 100,
+		multiplier: number = 1
+	): { explosion: Explosion; shockwave: Shockwave } {
+		if (!obj) {
+			return;
+		}
+
+		const explosion = new Explosion(obj.origin.x, obj.origin.y, size);
+		explosionCount++;
+
+		explosionScores.push(obj.score);
+
+		if (explosionScores.length > maxExplosionThreshold) {
+			explosionScores.pop();
+		}
+
+		if (explosionCount > maxExplosionCount) {
+			maxExplosionCount = explosionCount;
+
+			if (maxExplosionCount > maxExplosionThreshold) {
+				explosionCount = 0;
+				console.log("MAX DAMAGE ACHEIVEMENT");
+
+				this.setSlowMo(0.25, 4);
+
+				let bonus = 0;
+				explosionScores.forEach((v) => (bonus += v));
+				bonus *= 5;
+
+				const achievement = new Achievement(`MASSIVE DAMAGE`, bonus);
+				this.addScenery(achievement);
+				this.addScore(achievement, "achievement");
+
+				//const marker = new ScoreMarker(obj, `+${bonus}`);
+				//this.addScenery(marker);
+
+				// Track score of each explosion and display total points with achievement
+
+				maxExplosionThreshold += 10;
+			}
+		}
+
+		explosion.on("expired", () => {
+			explosionCount--;
+		});
+
+		this.addScenery(explosion);
+
+		const shockwave = new Shockwave(
+			obj.origin.x,
+			obj.origin.y,
+			obj.velocity,
+			size,
+			multiplier
+		);
+
+		shockwave.on("expired", () => {
+			this.shockwaves = this.shockwaves.filter((x) => x !== shockwave);
+		});
+
+		this.shockwaves.push(shockwave);
+
+		return {
+			explosion,
+			shockwave,
+		};
+	}
+
+	setSlowMo(time: number, factor: number) {
+		if (!this.slowMoTimer) {
+			this.slowMoTimer = new SlowMoTimer(time, factor);
+			this.slowMoTimer.on("expired", () => (this.slowMoTimer = null));
+		}
+	}
+
+	shipDestroyed() {
+		if (this.ship) {
+			// largeExplosion.play();
+			this.createExplosion(this.ship);
+			this.addFlash(5);
+			this.ship.destroy();
+			this.setSlowMo(0.25, 8);
+		}
+	}
+
+	alienDestroyed() {
+		if (this.alien) {
+			this.addFlash(5);
+			this.createExplosion(this.alien);
+			this.alien.destroy();
+		}
+	}
+
+	addFlash(frames: number) {
+		const flash = new Flash(frames);
+		this.addScenery(flash);
+	}
+
+	addScenery(obj: any) {
+		obj.on("expired", () => {
+			this.scenery = this.scenery.filter((x) => x !== obj);
+		});
+
+		this.scenery.push(obj);
+	}
+
+	rockDestroyed(rock: Rock, multiplier: number = 1) {
+		let boom = this.createExplosion(rock, rock.size * 5, multiplier);
+		let debris = rock.split();
+
+		this.rocks = this.rocks.filter((x) => x !== rock);
+		this.rocks.push(...debris);
+
+		this.shockwaves.forEach((shockwave) => {
+			shockwave.rocks = shockwave.rocks.filter((x) => x !== rock);
+		});
+
+		boom.shockwave.rocks = debris;
+
+		rock = null;
+	}
+
+	addAlien() {
+		const lvl = Math.min(this.level, 14);
+		let little = false;
+		let alienSound = largeAlien;
+
+		if (this.score >= 40000) {
+			little = true;
+		} else {
+			switch (lvl) {
+				case 7:
+					little = this.levelTimer > 60 && random(1, 3) === 2;
+					break;
+				case 8:
+					little = this.levelTimer > 30 && random(1, 10) % 2 === 0;
+					break;
+				default:
+					little = random(1, 10) <= lvl + 2;
+					break;
+			}
+		}
+
+		if (little) {
+			alienSound = smallAlien;
+			this.alien = new SmallAlien(this.ship);
+		} else {
+			this.alien = new BigAlien();
+		}
+
+		// alienSound.play();
+
+		this.alien.on("expired", () => {
+			// alienFire.stop();
+			// alienSound.stop();
+			// largeExplosion.play();
+			this.alien = null;
+			this.alienBullets.forEach((b) => b.destroy());
+			this.alienBullets.length = 0;
+		});
+
+		this.alien.on("fire", (alien, bullet: Bullet) => {
+			// alienFire.play();
+
+			bullet.on("expired", () => {
+				this.alienBullets = this.alienBullets.filter(
+					(x) => x !== bullet
+				);
+			});
+
+			this.alienBullets.push(bullet);
+		});
+	}
+
+	// TODO: remove second argument
+	addScore(obj: Object2D, name: string) {
+		console.log(obj.score, name);
+		this.score += obj.score;
+		this.extraLifeScore += obj.score;
+
+		if (this.extraLifeScore >= EXTRA_LIFE) {
+			this.lives++;
+			this.extraLifeScore -= EXTRA_LIFE;
+			// extraLife.play();
+		}
+
+		this.addScenery(new ScoreMarker(obj, `${obj.score}`));
+	}
+
+	addPowerup() {
+		// getPowerup.play();
+	}
+
+	shake() {
+		if (this.shakeTimer <= 0.0) {
+			this.shakeTimer = SHAKE_TIME;
+		}
+	}
+
+	tryPlaceShip(dt) {
+		this.shipTimer += dt;
+
+		if (this.shipTimer <= 2) {
+			return;
+		}
+
+		let rect: Rect = SHIP_RECT;
+
+		let collided = false;
+
+		this.rocks.forEach((rock) => {
+			collided = collided || rock.collided(rect);
+		});
+
+		if (this.alien) {
+			collided = collided || this.alien.collided(rect);
+		}
+
+		if (!collided) {
+			this.shipTimer = 0;
+			this.addShip(WIDTH / 2, HEIGHT / 2);
+		}
+	}
+
+	updateAlienTimer(dt: number) {
+		if (!this.alien) {
+			this.alienTimer -= dt;
+
+			if (this.alienTimer <= 0) {
+				this.addAlien();
+				this.alienTimer = random(10, 15);
+			}
+		}
+	}
+
+	shouldTryToPlaceShip(): boolean {
+		return !!this.shipTimer || (!this.ship && !!this.lives);
+	}
+
+	shouldCheckForNextLevel(): boolean {
+		return !this.rocks.length && !!this.lives;
+	}
+
+	shouldCheckCollisions(): boolean {
+		return !!this.ship || !!this.shipBullets.length;
+	}
 }
