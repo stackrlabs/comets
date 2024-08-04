e2812984612ee30fb8768b0c444e8e30b4659040
Aashutosh Rathi <aashutosh@stackrlabs.xyz>
2024-08-04 17:52:57 +0530

    fix screen size

--- game/src/core/constants.ts
+++ game/src/core/constants.ts
@@ -3,8 +3,8 @@ export let WIDTH = 960;
 export let HEIGHT = 720;
 
 export const updateScreenDimensions = (width: number, height: number) => {
-  WIDTH = width;
-  HEIGHT = height;
+  // WIDTH = width;
+  // HEIGHT = height;
 };
 
 export const OBJECT_SCALE = 0.75;
