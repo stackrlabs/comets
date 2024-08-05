dc706ab2cc6c9019c11a3073debac2a8a0c79002
Aashutosh Rathi <aashutosh@stackrlabs.xyz>
2024-08-05 22:28:49 +0530

    fix: add min width for img for small screen

--- game/src/components/past-games/da-image.tsx
+++ game/src/components/past-games/da-image.tsx
@@ -10,7 +10,7 @@ export const DAImage = (props: DAImageProps) => {
   const { da, width = 20, height = 20 } = props;
   return (
     <img
-      className="rounded-full"
+      className="rounded-full min-w-5 min-h-5"
       src={`https://assets.stackrlabs.xyz/${da}.png`}
       alt={DA_NAME[da]}
       width={width}
